0e0f9f56ff39b9734660185de9b191a518eb583a
SAEBONGCHON <saebongchon@gmail.com>
2023-02-22 14:01:17 +0900

    필터 생성함

---
+++ src/kh/common/filter/EncodingFilter.java
@@ -0,0 +1,21 @@
+package kh.common.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+public class EncodingFilter implements Filter {
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
